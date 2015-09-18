528b1b3b1b3f48d7deeaef96c820ef6e3547155a
datazen <scott.logsdon@loadedcommerce.com>
2015-09-18 12:27:31 -0400

    updated version

--- payment/Loaded_Payments/controller.php
+++ payment/Loaded_Payments/controller.php
@@ -45,11 +45,11 @@ class Loaded_Payments extends lC_Addon { // your addon must extend lC_Addon
    /**
     * The addon version
     */     
-    $this->_version = '1.0.1';
+    $this->_version = '1.0.2';
    /**
     * The Loaded 7 core compatibility version
     */     
-    $this->_compatibility = '7.002.2.0'; // the addon is compatible with this core version and later   
+    $this->_compatibility = '7.003.0.0'; // the addon is compatible with this core version and later   
    /**
     * The base64 encoded addon image used in the addons store listing
     */     
